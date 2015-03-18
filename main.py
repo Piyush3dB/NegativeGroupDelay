b81abae5da557bc48a91b0e6a7aa78d052f384e5
Piyush Singh <radprs@fulmar.hw.etv.eld.ericsson.se>
2015-03-18 09:20:47 +0000

    work in progress

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
 
 import numpy as np
-import 
+from scipy.signal import lfilter, tf2zpk
+
+
+b = np.array([1.0770389918072, 2.03115724017531, 0.961079950480885])
+a = np.array([1.0000000000000, 1.98606828810359, 0.992414110487639])
+
